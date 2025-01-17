36eb582e657e78f833e61133c70a1308a886f9cc
pzoch <piotr.zoch@gmail.com>
2025-01-17 15:48:24 +0100

    commit before het_rate

--- fortran_code/data.f90
+++ fortran_code/data.f90
@@ -70,8 +70,8 @@ call chdir(cwd_r)
      if (switch_wage_vs_income == 0) then
          
         !OPEN (unit=3, FILE = "_data_omega_deaton_avghourlyhh.txt")    
-         !Open(unit = 3, FILE = "_data_omega_busno_drop_hhslabinc_3_avghourlyhh.txt") 
-         Open(unit = 3, FILE = "_data_omega_mostdrop_hhslabinc_avghourlyhh.txt") 
+         Open(unit = 3, FILE = "_data_omega_busno_drop_hhslabinc_avghourlyhh.txt") 
+         !Open(unit = 3, FILE = "_data_omega_mostdrop_hhslabinc_avghourlyhh.txt") 
      elseif (switch_wage_vs_income == 1) then
          
         OPEN (unit=3, FILE = "_data_omega_deaton_hdslabinc.txt")    
@@ -155,8 +155,8 @@ call chdir(cwd_r)
     if (switch_wage_vs_income == 0) then
     
         !Open(unit = 8, FILE = "_data_sigma2eps_deaton_avghourlyhh.txt")  
-        !Open(unit = 8, FILE = "_data_sigma2eps_busno_drop_hhslabinc_3_avghourlyhh.txt")  
-        Open(unit = 8, FILE = "_data_sigma2eps_mostdrop_hhslabinc_avghourlyhh.txt")  
+        Open(unit = 8, FILE = "_data_sigma2eps_busno_drop_hhslabinc_avghourlyhh.txt")  
+        !Open(unit = 8, FILE = "_data_sigma2eps_mostdrop_hhslabinc_avghourlyhh.txt")  
     
     elseif (switch_wage_vs_income == 1) then
         
--- fortran_code/main.f90
+++ fortran_code/main.f90
@@ -20,8 +20,8 @@ program olg2
     
 
         
-    version = 'ndel_' ! these three strings allow us to load a correct version
-    experiment = 'ndm_'
+    version = 'nstr_' ! these three strings allow us to load a correct version
+    experiment = 'all_'
     closure = 'govt__'
     
     call globals         ! globals is a subroutine in global_vars2 module             
